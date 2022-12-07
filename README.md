a288f356555ec7ea492e024fdb80a023a6dc511a
Kanstant <kanstantsin_budzko@epam.com>
2022-12-07 20:00:54 +0100

    ready to run

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Testing Custom Action
+# action-test
+## Inputs
+
+### `GITHUB_TOKEN`
+
+**Required** The GH Token used to access the API.
+
+### `the_input`
+
+**Optional** The Input string to ptint. Default: ''
+
+## Outputs
+
+### `the_output`
+
+just the output string.
+
+## Example usage
+
+```yaml
+uses: kanstant/action-test@main
+with:
+  GITHUB_TOKEN: '12345'
+  the_input: 'Here is the Input string'
+```
---
+++ index.js
@@ -0,0 +1,16 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const ghToken = core.getInput('GITHUB_TOKEN');
+  const the_input = core.getInput('the_input');
+
+  console.log(`This is The_input ${the_input}!`);
+  const the_time = (new Date()).toTimeString();
+  core.setOutput("time", the_time);
+  //JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
